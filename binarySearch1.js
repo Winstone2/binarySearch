bda20fa0aaef0cf52a44e06fcda483797ef0a76b
Winstone <you@example.com>
2023-07-21 23:17:28 +0300

    initial commit

---
+++ binarySearch1.js
@@ -0,0 +1,18 @@
+function binarySearch1 (arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) {
+            return mid; // Element found, return its index
+        } else if (arr[mid] < target) {
+            left = mid + 1; // Search in the right half
+        } else {
+            right = mid - 1; // Search in the left half
+        }
+    }
+
+    return -1; // Element not found
+}
